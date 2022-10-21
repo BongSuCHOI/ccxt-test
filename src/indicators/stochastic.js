882a4e36cab97bbb3736e210709b4b91835abada
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-10-21 15:47:18 +0900

    add : stoch 지표 추가

--- src/indicators/stochastic.js
+++ src/indicators/stochastic.js
@@ -5,27 +5,82 @@
 const { SMACalc } = require('./calc/sma');
 const { STOCHcalc } = require('./calc/stoch');
 
-exports.SLOW_STOCH = (
-	datas,
-	length = 14,
-	overBuy = 80,
-	overSell = 20,
-	smoothK = 3,
-	smoothD = 3
-) => {
-	const stochArr = [];
+exports.SLOW_STOCH = (datas, length = 14, bought = 80, sold = 20, smoothK = 3, smoothD = 3) => {
+	/**
+	 * datas = 캔들 정보 배열 (fetchOHLCV() 리턴값)
+	 * len = 캔들 갯수
+	 * n = 뒤에서(최신 데이터)부터 자를 길이
+	 * ex) datas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; len = 5; n = 2;
+	 * res) return = [4, 5, 6, 7, 8]
+	 */
+	const getStoch = (datas, len, n) => {
+		const sliceDatas = datas.slice(-len - n, datas.length - n);
+		const highs = sliceDatas.map((data) => data[2]);
+		const lows = sliceDatas.map((data) => data[3]);
+		const closes = sliceDatas.map((data) => data[4]);
+		return STOCHcalc(highs, lows, closes, len);
+	};
 
-	for (let i = 0; i < smoothK; i++) {
-		const sliceDatas = datas.slice(-length - i, datas.length - i);
-		const highDatas = sliceDatas.map((data) => data[2]);
-		const lowDatas = sliceDatas.map((data) => data[3]);
-		const closeDatas = sliceDatas.map((data) => data[4]);
-		const stoch = STOCHcalc(highDatas, lowDatas, closeDatas, length);
-		stochArr.push(stoch);
+	/**
+	 * n = 최신 데이터부터 몇개 뽑을지 (n = 1 ? 최신 데이터 k + 이전 데이터 k)
+	 * return되는 k는 총 3개.
+	 * ex) n = 0 ? return = [1, 2, 3] : n = 1 ? return = [2, 3, 4]
+	 * 현재 d를 뽑기 위해선 현재 k와 이전 k 합쳐서 3개가 필요.
+	 * 반대로 이전 d를 뽑기 위해선 이전 k와 더 이전 k 합쳐서 3개가 필요.
+	 */
+	const getK = (n) => {
+		const kArr = [];
+
+		for (let i = n; i < smoothD + n; i++) {
+			const stochArr = [];
+
+			for (let j = i; j < smoothK + i; j++) {
+				const stoch = getStoch(datas, length, j);
+				stochArr.push(stoch);
+			}
+
+			kArr.push(SMACalc(stochArr, smoothK));
+		}
+
+		return kArr;
+	};
+
+	// 현재 k 값 (smoothD의 숫자만큼)
+	const currKArr = getK(0);
+
+	// 이전 k 값 (smoothD의 숫자만큼)
+	const prevKArr = getK(1);
+
+	const currK = currKArr[0];
+	const prevK = prevKArr[0];
+	const currD = SMACalc(currKArr, smoothD);
+	const prevD = SMACalc(prevKArr, smoothD);
+
+	let goldCross = false;
+	let deadCross = false;
+	let OverSold = false;
+	let OverBought = false;
+
+	// gold cross (currK > currD && prevK < prevD)
+	if (currK > currD && prevK < prevD) {
+		goldCross = true;
 	}
 
-	const k = SMACalc([stochArr], smoothK);
-	// D부터 꼬였는데 아래처럼 D도 k의 이전 3개 값 구해서 하면 될 듯..?
-	const d = SMACalc([79.49, 84.99, 92.94], smoothD);
-	console.log(k, d);
+	// dead cross (currK < currD && prevK > prevD)
+	if (currK < currD && prevK > prevD) {
+		deadCross = true;
+	}
+
+	// over sold
+	if (currK < sold) {
+		OverSold = true;
+	}
+
+	// over bought
+	if (currK > bought) {
+		OverBought = true;
+	}
+
+	// return { currK, prevK, currD, prevD };
+	return { goldCross, deadCross, OverSold, OverBought };
 };
